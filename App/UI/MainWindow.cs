83be55d8f1df387289144baccd3e58def21a1e36
AndrewKindleysides <andrew.kindleysides@advancedcomputersoftware.com>
2015-04-29 16:31:44 +0100

    renamed classes and removed redundant code

---
+++ App/UI/MainWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
