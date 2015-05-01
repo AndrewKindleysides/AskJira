4cc6f5cb849dd5dce9ef9ea15157a24dabd14878
Andrew Kindleysides <andrew.kindleysides@advancedcomputersoftware.com>
2015-05-01 13:16:18 +0100

    fixed a few layout issues with resizing

--- App/UI/MainWindow.Designer.cs
+++ App/UI/MainWindow.Designer.cs
@@ -30,21 +30,25 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.jiraGrid = new System.Windows.Forms.DataGridView();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.OrderNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.JiraLink = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.SummaryText = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.DateCreated = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ClientName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.searchBox = new System.Windows.Forms.TextBox();
-            this.searchButton = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.jiraGrid)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // jiraGrid
             // 
-            this.jiraGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.jiraGrid.AllowUserToOrderColumns = true;
+            this.jiraGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.jiraGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.jiraGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.jiraGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.OrderNumber,
@@ -55,39 +59,8 @@
             this.jiraGrid.Location = new System.Drawing.Point(12, 103);
             this.jiraGrid.Name = "jiraGrid";
             this.jiraGrid.RowHeadersVisible = false;
-            this.jiraGrid.Size = new System.Drawing.Size(1172, 522);
+            this.jiraGrid.Size = new System.Drawing.Size(965, 522);
             this.jiraGrid.TabIndex = 0;
-            // 
-            // OrderNumber
-            // 
-            this.OrderNumber.HeaderText = "Order";
-            this.OrderNumber.Name = "OrderNumber";
-            this.OrderNumber.ReadOnly = true;
-            this.OrderNumber.Width = 58;
-            // 
-            // JiraLink
-            // 
-            this.JiraLink.HeaderText = "Jira";
-            this.JiraLink.Name = "JiraLink";
-            this.JiraLink.Width = 48;
-            // 
-            // SummaryText
-            // 
-            this.SummaryText.HeaderText = "Summary";
-            this.SummaryText.Name = "SummaryText";
-            this.SummaryText.Width = 75;
-            // 
-            // DateCreated
-            // 
-            this.DateCreated.HeaderText = "Date Created";
-            this.DateCreated.Name = "DateCreated";
-            this.DateCreated.Width = 95;
-            // 
-            // ClientName
-            // 
-            this.ClientName.HeaderText = "Client";
-            this.ClientName.Name = "ClientName";
-            this.ClientName.Width = 58;
             // 
             // searchBox
             // 
@@ -109,20 +82,50 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = global::UI.Resources.logo;
-            this.pictureBox1.Location = new System.Drawing.Point(1034, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(827, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(150, 84);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
+            // OrderNumber
+            // 
+            this.OrderNumber.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.OrderNumber.HeaderText = "Order";
+            this.OrderNumber.Name = "OrderNumber";
+            this.OrderNumber.ReadOnly = true;
+            this.OrderNumber.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.OrderNumber.Width = 58;
+            // 
+            // JiraLink
+            // 
+            this.JiraLink.HeaderText = "Jira";
+            this.JiraLink.Name = "JiraLink";
+            // 
+            // SummaryText
+            // 
+            this.SummaryText.HeaderText = "Summary";
+            this.SummaryText.Name = "SummaryText";
+            // 
+            // DateCreated
+            // 
+            this.DateCreated.HeaderText = "Date Created";
+            this.DateCreated.Name = "DateCreated";
+            // 
+            // ClientName
+            // 
+            this.ClientName.HeaderText = "Client";
+            this.ClientName.Name = "ClientName";
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1196, 634);
+            this.ClientSize = new System.Drawing.Size(989, 634);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.searchButton);
             this.Controls.Add(this.searchBox);
